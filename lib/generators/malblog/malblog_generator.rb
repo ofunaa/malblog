0b7e58cb1958cab5415f6efc7875a8cbe0304770
takujifunao <takuji.funao@gmail.com>
2015-11-23 12:24:47 +0900

    version update

--- lib/generators/malblog/malblog_generator.rb
+++ lib/generators/malblog/malblog_generator.rb
@@ -1,4 +1,4 @@
-class MalblogGenerators < Rails::Generators::NamedBase
+class MalblogGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def test_dei
